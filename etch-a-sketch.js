0a7797aef8e4fdbea275c9ed03cf73fc70469dca
Nick-Hericks <nickhericks@gmail.com>
2020-02-26 19:48:32 +0200

    randomize color on shake

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -15,22 +15,28 @@ const { width, height } = canvas;
 // console.log(width, height);
 
 // create random x and y starting points on the canvas
-let x = Math.floor(Math.random() * width);
-let y = Math.floor(Math.random() * height);
-
-
+let x;
+let y;
+let hue;
 
 ctx.lineJoin = 'round';
 ctx.lineCap = 'round';
 ctx.lineWidth = MOVE_AMOUNT;
 
-let hue = 0;
-ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
-ctx.beginPath(); // start the drawing
-ctx.moveTo(x, y);
-ctx.lineTo(x, y);
-ctx.stroke();
+function begin() {
+	x = Math.floor(Math.random() * width);
+	y = Math.floor(Math.random() * height);
+
+	hue = 0;
+  ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+
+	ctx.beginPath(); // start the drawing
+	ctx.moveTo(x, y);
+	ctx.lineTo(x, y);
+	ctx.stroke();
+}
+
 
 // Write a draw function
 function draw( { key } ) {
@@ -39,8 +45,7 @@ function draw( { key } ) {
 	// update color of
 	ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
 
-
-	console.log(key);
+	// console.log(key);
 	ctx.beginPath();
 	ctx.moveTo(x, y);
 	
@@ -72,14 +77,25 @@ function handleKey(e) {
 		e.preventDefault();
 		draw({ key: e.key });
 		// console.log(e.key);
-
 	}
 }
 
 
 // clear/shake function
+function clearCanvas() {
+	canvas.classList.add('shake');
+	ctx.clearRect(0, 0, width, height);
+	canvas.addEventListener('animationend', function() {
+		canvas.classList.remove('shake');
+	}, { once: true });
+	begin();
+}
 
+shakeButton.addEventListener('click', clearCanvas);
 
 
 // Listen for arrow keys
 window.addEventListener('keydown', handleKey);
+
+
+begin();
